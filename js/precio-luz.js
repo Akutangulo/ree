6e7aa7bcc9370109fc1cf27f632eecf76fd20397
Akutangulo <akutangulo@gmail.com>
2024-05-03 04:01:05 +0200

    Update precio-luz.js

--- js/precio-luz.js
+++ js/precio-luz.js
@@ -147,6 +147,7 @@ function formatDateInCustomFormat(date) {
     const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     return date.toLocaleDateString('es-ES', options);
 }
+
 // Función para manejar el cambio en el estado del botón-checkbox
 function togglePrecioPasado() {
     const checkbox = document.getElementById("precioLuzPasado");
